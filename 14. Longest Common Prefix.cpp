6475b1c0a5cac6d10bb1867411f6f36aea23698b
Abhishek kumar <112111938+abishekk-jpg@users.noreply.github.com>
2023-01-06 21:26:00 +0530

    leetcode 14

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        sort(strs.begin(), strs.end());
+        string a = strs[0];
+        string b = strs[n-1];
+        string ans = "";
+        for(int i=0;i<a.size();i++){
+            if(a[i]==b[i]){
+                ans += a[i];
+            }
+            else{
+                break;
+            }
+        }
+        if(ans.size()==0){
+            return "";
+        }
+        else{
+            return ans;
+        }
+    }
+};
